a8442b1bd3c504ff5eaff507c2e6290923ba53c9
dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2020-01-02 17:21:36 -0500

    dark theme stuff and preliminary 12h clock work

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+/*Config*/
+// DarkMode
+var darkMode=true;
+// 24 hour mode clock
+var mode24 = false;
+
+
+
+//-------------------------------------//
 var clock = {
   el: {},
   config: {
@@ -45,62 +54,78 @@ var clock = {
         clockHtml += '</div>';
       }
       clock.el.clock.innerHTML = clockHtml;
-      clock.fn.domReferences();      
-      
+      clock.fn.domReferences();
+
       /*clock.fn.drawDigits("------");*/
-      clock.fn.loop();    
-      setTimeout(clock.fn.loop,0);      
+      clock.fn.loop();
+      setTimeout(clock.fn.loop,0);
       setInterval(clock.fn.loop,clock.config.refreshInterval);
-      
+
       console.log("DOM elements created.");
     },
     drawDigits: function(str) {
-      
+
       console.log("Draw "+str);
-      
+
       if (str && str.length) {
         for (var i=0; i<str.length; i++) {
-          if (clock.el.digits[i]) { 
+          if (clock.el.digits[i]) {
             if (str[i] !== clock.el.digits[i].getAttribute("display")) {
               clock.el.digits[i].setAttribute("display",str[i]);
             } else {
               if ( i >= 2) {
                 if (clock.el.digits[i].getAttribute("reverse") != "true") {
-                  console.log("Same digit at position "+ i + " ("+str[i]+")");   
+                  console.log("Same digit at position "+ i + " ("+str[i]+")");
                   clock.el.digits[i].setAttribute("reverse","true");
-                } else {  
-                  console.log("Same digit at position "+ i + " ("+str[i]+")");  
+                } else {
+                  console.log("Same digit at position "+ i + " ("+str[i]+")");
                   clock.el.digits[i].setAttribute("reverse","false");
-                }                  
+                }
               }
-
             }
           }
         }
       }
     },
     getTimeString: function() {
-      
+
       var addZero = function(i) {
         if (i < 10) {
           i = "0" + i;
         }
         return i;
-      }  
-
+      }
       var d = new Date();
-      var h = addZero(d.getHours()).toString();
-      var m = addZero(d.getMinutes()).toString();
-      var s = addZero(d.getSeconds()).toString();
-     
-      return( h + m + s);
 
+      var m = addZero(d.getMinutes()).toString();
+
+      if(mode24){
+          var h = addZero(d.getHours()).toString();
+          return( h + m);
+      }else{
+        var h =d.getHours();
+        console.log(h);
+        if(h>13)
+          h=h%12;
+        if(h==0)
+          h=12;
+        h = addZero(h.toString());
+        return( h + m );
+      }
     },
     loop: function() {
-      var str = clock.fn.getTimeString();      
-      clock.fn.drawDigits(str);        
+      var str = clock.fn.getTimeString();
+      clock.fn.drawDigits(str);
     }
   }
 };
 clock.fn.init();
-var a = true;
+if(darkMode){
+  document.querySelector('html').style.backgroundColor="#5D5D5D";
+  for(var x of document.querySelectorAll(".line")){
+	   x.classList.toggle("darkline")
+  }
+  for(var x of document.querySelectorAll(".pixel")){
+	   x.classList.toggle("dark")
+  }
+}
